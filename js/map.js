38ea2c2cb2671c646a11e74f338bcb3354e67c25
Brandon Liu <bdon@bdon.org>
2014-02-02 21:47:54 -0800

    change bg to san francisco photo. switch to toner map.

--- js/map.js
+++ js/map.js
@@ -1,5 +1,5 @@
 var map = L.map('map_div').setView([37.782254,-122.39124], 13);
-    L.tileLayer('http://{s}.tile.cloudmade.com/aa0e217a6d1d4c6a847dbb5de74b0227/997/256/{z}/{x}/{y}.png', {
+    L.tileLayer('http://tile.stamen.com/toner/{z}/{x}/{y}.png', {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
     maxZoom: 18
 }).addTo(map);
