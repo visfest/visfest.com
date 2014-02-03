1e320e15150fdc74d77f4426e98bfab25c4db87e
Brandon Liu <bdon@bdon.org>
2014-02-02 22:55:06 -0800

    fix map tile attribution

--- js/map.js
+++ js/map.js
@@ -1,6 +1,6 @@
 var map = L.map('map_div').setView([37.782254,-122.39124], 13);
     L.tileLayer('http://tile.stamen.com/toner/{z}/{x}/{y}.png', {
-    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
+    attribution: 'Map tiles by <a target="_top" href="http://stamen.com">Stamen Design</a>, under <a target="_top" href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a target="_top" href="http://openstreetmap.org">OpenStreetMap</a>, under <a target="_top" href="http://creativecommons.org/licenses/by-sa/3.0">CC BY SA</a>',
     maxZoom: 18
 }).addTo(map);
 L.marker([37.782254,-122.39124]).addTo(map)
