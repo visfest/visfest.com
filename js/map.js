f8dcb9a98f40cc1c93214101aa9dea52851737ab
Brandon Liu <bdon@bdon.org>
2014-02-04 21:08:40 -0800

    move to template site

--- js/map.js
+++ js/map.js
@@ -1,7 +1,5 @@
-var map = L.map('map_div').setView([37.782254,-122.39124], 13);
-    L.tileLayer('http://tile.stamen.com/toner/{z}/{x}/{y}.png', {
-    attribution: 'Map tiles by <a target="_top" href="http://stamen.com">Stamen Design</a>, under <a target="_top" href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a target="_top" href="http://openstreetmap.org">OpenStreetMap</a>, under <a target="_top" href="http://creativecommons.org/licenses/by-sa/3.0">CC BY SA</a>',
-    maxZoom: 18
-}).addTo(map);
+var map = L.mapbox.map('map-canvas','bdon.h6c9omjp').setView([37.782254,-122.39124], 14);
+
 L.marker([37.782254,-122.39124]).addTo(map)
   .bindPopup('GitHub HQ')
+
