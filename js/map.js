7fe15b0992ab32694a7075e805d5bc2779188889
Brandon Liu <bdon@bdon.org>
2014-02-05 22:20:38 -0800

    disable mouse map scroll

--- js/map.js
+++ js/map.js
@@ -1,4 +1,4 @@
-var map = L.map('map-canvas').setView([37.782254,-122.39124], 13);
+var map = L.map('map-canvas', {scrollWheelZoom:false}).setView([37.782254,-122.39124], 13);
     L.tileLayer('http://tile.stamen.com/terrain/{z}/{x}/{y}.png', {
     attribution: 'Map tiles by <a target="_top" href="http://stamen.com">Stamen Design</a>, under <a target="_top" href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a target="_top" href="http://openstreetmap.org">OpenStreetMap</a>, under <a target="_top" href="http://creativecommons.org/licenses/by-sa/3.0">CC BY SA</a>',
     maxZoom: 18
