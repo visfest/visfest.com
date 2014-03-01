9740495b61e794db1305edac603969c94d95f5e8
Brandon Liu <bdon@bdon.org>
2014-03-01 14:18:28 -0800

    switch back to pink map

--- js/map.js
+++ js/map.js
@@ -1,8 +1,4 @@
-var map = L.map('map-canvas', {scrollWheelZoom:false}).setView([37.782254,-122.39124], 13);
-    L.tileLayer('http://tile.stamen.com/terrain/{z}/{x}/{y}.png', {
-    attribution: 'Map tiles by <a target="_top" href="http://stamen.com">Stamen Design</a>, under <a target="_top" href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a target="_top" href="http://openstreetmap.org">OpenStreetMap</a>, under <a target="_top" href="http://creativecommons.org/licenses/by-sa/3.0">CC BY SA</a>',
-    maxZoom: 18
-}).addTo(map);
+var map = L.mapbox.map('map-canvas','bdon.h6c9omjp', {scrollWheelZoom:false}).setView([37.782254,-122.39124], 14);
 
 L.marker([37.782254,-122.39124]).addTo(map)
   .bindPopup('GitHub HQ')
