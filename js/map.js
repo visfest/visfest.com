544de754c782639dcea0c9722c5bcafb9bf45daf
Ian Johnson <enjalot@gmail.com>
2015-09-13 18:08:34 -0700

    update site to reflect 2015 event

--- js/map.js
+++ js/map.js
@@ -1,5 +1,16 @@
-var map = L.mapbox.map('map-canvas','bdon.h6c9omjp', {scrollWheelZoom:false}).setView([37.782254,-122.39124], 14);
+try {
+var map2014 = L.mapbox.map('map-canvas-2014','bdon.h6c9omjp', {scrollWheelZoom:false}).setView([37.782254,-122.39124], 14);
 
-L.marker([37.782254,-122.39124]).addTo(map)
+L.marker([37.782254,-122.39124]).addTo(map2014)
   .bindPopup('GitHub HQ')
+} catch(e) {
+}
 
+
+try {
+var map2015 = L.mapbox.map('map-canvas-2015','bdon.h6c9omjp', {scrollWheelZoom:false}).setView([37.7543922,-122.4185027], 16);
+
+L.marker([37.7543922,-122.4185027]).addTo(map2015)
+  .bindPopup('GAFFTA')
+} catch(e) {
+}
